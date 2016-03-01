98eeaea6d774dcfc7c79ba57f4a5e7f3cd5682a0
Frederic Lopes Goncalves Magalhaes <fredericlopesgoncalvesmagalhaes@MacBook-Pro-de-Frederic.local>
2016-03-01 23:26:15 +0100

    20160301 - Page de connexion terminée. Le tableau de bord est construit ainsi que certains onglets du MenuStrip.

---
+++ ProjetGL_FREDERICLOPES/MainClass.vb
@@ -0,0 +1,11 @@
+﻿Public Class MainClass
+    Private fmLogin As frmMain
+
+    Public Shared Sub Main()
+
+    End Sub
+
+    Public Sub close()
+        Me.close()
+    End Sub
+End Class
---
+++ ProjetGL_FREDERICLOPES/frmTB.vb
@@ -0,0 +1,16 @@
+﻿Public Class frmTB
+
+    Private Sub msiCoobâtirQuitter_Click(sender As Object, e As EventArgs) Handles msiCoobâtirQuitter.Click
+        Dim res = MsgBox(
+            "Vous êtes sur le point de quitter l'exécution du programme, 
+            voulez-vous vraiment quitter COOBÂTIR ?",
+            MsgBoxStyle.YesNo + MsgBoxStyle.DefaultButton2, "Quitter l'application")
+        If res = MsgBoxResult.Yes Then
+            frmMain.Close()
+        End If
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgbTb.CellContentClick
+
+    End Sub
+End Class
